6cba9c25172880bc967db4aad49e83e22587e187
Edleron <Edleron@hotmail.com>
2023-01-17 04:36:15 +0300

    commit user name changed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Edleron Git repos");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
