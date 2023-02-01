027fe80970231cccb2520e3cfa29e525f80400f5
Edleron <Edleron@hotmail.com>
2023-02-01 21:06:57 +0300

    adding enemy movement

---
+++ Assets/Scripts/EnemyMovements.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovements : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3.0f;
+    Rigidbody2D myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+}
